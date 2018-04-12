88d1c1dee5075ba0f53a5ecfe7b59682204b539b
Uli Horn <ulihorn@gmail.com>
2018-04-12 17:58:39 +0200

    removed - from comment on line 43 in application/puzzlecommon/models/crossword.js

--- applications/puzzle/common/models/crossword.js
+++ applications/puzzle/common/models/crossword.js
@@ -39,7 +39,7 @@ module.exports = function(Crossword) {
       etcd.delSync("puzzle");
       Crossword.findOne(function (err, crossword) {
         
-        // Part 4:- Uncomment the next line to enable puzzle pod highlighting when clicking the Submit button
+        // Part 4: Uncomment the next line to enable puzzle pod highlighting when clicking the Submit button
         fireHit();
         if (err) handleError(err.message, cb);
         for (var j = 0; j < words.length; j++) {
